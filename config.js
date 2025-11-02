89af4221e3cb55cd6af3fe0b93d85f10ec688f74
Sunny02 <danghaiyen.fbf@gmail.com>
2025-11-02 20:07:10 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+// File: config.js
+
+const config = {
+  // Tên người nhận (trong thư)
+  recipientName: "Thúy Quỳnh",
+
+  // Lời nhắn (dùng dấu ` backtick này để viết được nhiều dòng)
+  letterMessage: `
+Chúc bé iu của anh sinh nhật vui vẻ và mọi điều tốt lành sẽ đến với em. Anh hi vọng mình được có mặt trong tất các các sinh nhật của em sau này. Hãy để anh được yêu thương che chở và mang lại hạnh phúc cho em. Yêu em nhiều lắm lắm.♥️`,
+
+  // Ảnh ở màn hình đăng nhập (page 1)
+  passwordImage: "./image/a1.jpg",
+  
+  // Lời nhắn alert khi nhập sai mật khẩu
+  wrongPasswordAlert: "NÀO NÀO CHẲNG LẺ MÂT KHẨU KHÔNG NHỚ SAO"
+};
